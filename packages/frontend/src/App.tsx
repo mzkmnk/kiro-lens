d12ace2dc1e781d2d7ab63856a520c746346bfae
mizuki <sma0729mex@gmail.com>
2025-09-05 01:06:16 +0900

    feat: shadcn/uiセットアップ完了 (#18)

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <main>
       <h1 className='text-2xl font-bold'>Kiro Lens</h1>
       <p>最低限の実装</p>
+      <Button>Click</Button>
     </main>
   );
 }
