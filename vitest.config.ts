7beacf6d9bbc26259596dfd80101c9238a0d6b43
mzkmnk <sma0729mex@gmail.com>
2025-09-06 17:27:20 +0900

    refactor: コマンドに対応

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-    exclude: ['node_modules', 'packages'],
-    environment: 'node',
-  },
-});
