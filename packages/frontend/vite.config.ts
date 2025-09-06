8701d8ba3c5b1669c8de4cbc741412a5ce5fcca6
mzkmnk <sma0729mex@gmail.com>
2025-09-06 11:14:01 +0900

    style: unused import

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
-import type { ViteEnv } from './src/vite-env';
 
 export default defineConfig(_ => {
   return {
