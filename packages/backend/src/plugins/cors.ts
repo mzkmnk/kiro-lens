f953f737b059ea6b4d40d342459f570318121522
mzkmnk <sma0729mex@gmail.com>
2025-09-07 17:57:22 +0900

    feat: corsエラーの設定

--- packages/backend/src/plugins/cors.ts
+++ packages/backend/src/plugins/cors.ts
@@ -3,7 +3,9 @@ import cors from '@fastify/cors';
 
 export async function corsPlugin(fastify: FastifyInstance, _options: FastifyPluginOptions) {
   await fastify.register(cors, {
-    origin: true, // 開発環境では全てのオリジンを許可
+    origin: ['http://localhost:3000'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 }
