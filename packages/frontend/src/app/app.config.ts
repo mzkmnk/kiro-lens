c7d7dc02f11093a7b4edd98df56972d4b57231d8
mzkmnk <sma0729mex@gmail.com>
2025-09-13 21:38:05 +0900

    feat: primeng style and theme

--- packages/frontend/src/app/app.config.ts
+++ packages/frontend/src/app/app.config.ts
@@ -2,11 +2,14 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
+} from "@angular/core";
+import { provideRouter } from "@angular/router";
 
-import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app.routes';
+import { providePrimeNG } from "primeng/config";
+
+import { provideHttpClient } from "@angular/common/http";
+import { routes } from "./app.routes";
+import { Noir } from "./noir-theme";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +17,10 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Noir,
+      },
+    }),
   ],
 };
